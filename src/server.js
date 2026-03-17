1a5741b08e9f1fe26282ecf80f818b5aae76e1a6
LongKun <longdh.udu.ptit@gmail.com>
2026-03-17 21:19:49 +0700

    update

--- src/server.js
+++ src/server.js
@@ -3,12 +3,23 @@ import bodyParser from 'body-parser';
 import viewEngine from './config/viewEngine.js';
 import initWebRoutes from './route/web.js';
 import connectDB from './config/connectDB.js';
+import cors from 'cors';
 require('dotenv').config();
 
 let app = express();
 
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
+
+app.options("*", cors());
+
+
 app.use (bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 viewEngine(app);
 initWebRoutes(app);
