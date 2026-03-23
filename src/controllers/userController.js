4d258f1b4b399b384078e52b0f08bec3af7d11ce
LongKun <longdh.udu.ptit@gmail.com>
2026-03-23 22:45:19 +0700

    read

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,40 +1,44 @@
-import userService from '../services/userService';
+import userService from "../services/userService";
 let handleController = async (req, res) => {
-    let email = req.body.email;
-    let password = req.body.password;
+  let email = req.body.email;
+  let password = req.body.password;
 
-    if (!email || !password) {
-        return res.status(500).json({
-            errCode: 1,
-            message: "Missing input parameter!"
-        })
-    }
+  if (!email || !password) {
+    return res.status(500).json({
+      errCode: 1,
+      message: "Missing input parameter!",
+    });
+  }
 
-    let userData = await userService.handleUserLogin(email, password);
-    return res.status(200).json({
-        errCode: userData.errCode,
-        message: userData.Message,
-        user: userData.user ? userData.user : {}
-    })
-}
+  let userData = await userService.handleUserLogin(email, password);
+  return res.status(200).json({
+    errCode: userData.errCode,
+    message: userData.Message,
+    user: userData.user ? userData.user : {},
+  });
+};
 
 let handleGetAllUsers = async (req, res) => {
-    let id = req.query.id;
+  let id = req.query.id;
 
-    if(!id) {
-        return res.status(200).json({
-            errCode: 1,
-            message: "Missing input parameter!",
-            users: []
-        })
-    }
+  if (!id) {
+    return res.status(200).json({
+      errCode: 1,
+      message: "Missing input parameter!",
+      users: [],
+    });
+  }
 
-    let users = await userService.getAllUsers(id);
+  let users = await userService.getAllUsers(id);
 
-}
-
+  return res.status(200).json({
+    errCode: 0,
+    message: "OK",
+    users: users,
+  });
+};
 
 module.exports = {
-    handleLogin: handleController,
-    handleGetAllUsers: handleGetAllUsers
-}
+  handleLogin: handleController,
+  handleGetAllUsers: handleGetAllUsers,
+};
