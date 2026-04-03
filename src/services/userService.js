c90e932abded3c6db8771546e49ec0b5d4032279
LongKun <longdh.udu.ptit@gmail.com>
2026-04-03 16:55:37 +0700

    create

--- src/services/userService.js
+++ src/services/userService.js
@@ -106,26 +106,26 @@ let createNewUser = (data) => {
       if (check === true) {
         resolve({
           errCode: 1,
-          message:
+          errMessage:
             "The email is already used by another user. Please use a different email!",
         });
+      } else {
+        let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+        await db.User.create({
+          email: data.email,
+          password: hashPasswordFromBcrypt,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          address: data.address,
+          phoneNumber: data.phoneNumber,
+          gender: data.gender === 1 ? true : false,
+          roleId: data.roleId,
+        });
+        resolve({
+          errCode: 0,
+          message: "OK",
+        });
       }
-
-      let hashPasswordFromBcrypt = await hashUserPassword(data.password);
-      await db.User.create({
-        email: data.email,
-        password: hashPasswordFromBcrypt,
-        firstName: data.firstName,
-        lastName: data.lastName,
-        address: data.address,
-        phoneNumber: data.phoneNumber,
-        gender: data.gender === 1 ? true : false,
-        roleId: data.roleId,
-      });
-      resolve({
-        errCode: 0,
-        message: "OK",
-      });
     } catch (e) {
       reject(e);
     }
